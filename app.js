ce2a7df3085b5606d21eb163ec49b9f2d8bdd134
jonathan-crepeau <jon.crepeau@gmail.com>
2025-02-06 19:48:49 -0800

    Excited Kitten question completed

--- app.js
+++ app.js
@@ -2,8 +2,22 @@
 
 // • Get Even: Write a for loop that will log only the even numbers in 0 through 200:
 
-for (let i = 0; i < 201; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = 0; i < 201; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// • Excited Kitten: Write code that logs "Love me, pet me! HSSSSSS!" 20 times. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+
+// for (let i = 0; i < 20; i++) {
+//     const additionalResponses = ["Human, why are you taking pictures of me?", "The catnip made me do it.", "Why does the red dot always get away?"];
+//   console.log("Love me, pet me! HISSS!");
+//   if (i % 2 === 0) {
+//     console.log(additionalResponses[Math.floor(Math.random() * 3)])
+//   }
+// }
+
+
+// • 
