35365fd7b5fccb30318a1bc0d06a8ba553eb1729
jonathan-crepeau <jon.crepeau@gmail.com>
2025-02-06 19:42:15 -0800

    Get Even question completed

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('All good under the hood');
+// console.log('All good under the hood');
+
+// • Get Even: Write a for loop that will log only the even numbers in 0 through 200:
+
+for (let i = 0; i < 201; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
