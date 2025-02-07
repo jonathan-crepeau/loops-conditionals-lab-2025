9c28121d9f226c376024bb6cb9a95debf9ed2f87
jonathan-crepeau <jon.crepeau@gmail.com>
2025-02-06 20:29:48 -0800

    Getting to Know You

--- app.js
+++ app.js
@@ -42,3 +42,48 @@
 // }
 
 // secondLogFizzBuzz();
+
+
+// • Getting To Know You: Use the following arrays to answer the questions below (name, age, hometown):
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+// 1. im Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the kenny array and replace it with "Gameboy".
+
+// kenny[0] = "Gameboy";
+// console.log(kenny);
+
+// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+
+// jimClark[1] += 1;
+// console.log(jimClark);
+
+// 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+
+// ryan[2] = "Gotham City";
+// console.log(ryan);
+
+// 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+
+// reuben.splice(2, 1, 'Chicago');
+// console.log(reuben);
+
+// reuben.pop();
+// reuben.push('Chicago')
+// console.log(reuben);
+
+// 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+
+// jimHaff.splice(2, 1, 'Portland, ME', 'Belfast, IR', 'Brooklyn, NY');
+// console.log(jimHaff);
+
+// 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim Haff is now, remove it from the array using .splice()
+
+// jimHaff.splice(2, 1, 'Portland, ME', 'Belfast, IR', 'Brooklyn, NY');
+
+// jimHaff.splice(2, 1);
+// console.log(jimHaff);
